93c00be936de09c1e7dd6ada7d5419dd01ced985
Marc Gorzala <marc@dancier.net>
2026-03-10 20:15:50 +0100

    new config, tango and functional training

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,3 +32,11 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
+
+import os
+
+# Dynamically add all subdirectories in content to STATIC_PATHS
+STATIC_PATHS = ["images"] + [d for d in os.listdir("content") if os.path.isdir(os.path.join("content", d))]
+
+# Automatically copy static files from article folders (e.g., images in content/foo/)
+ARTICLE_STATIC_CREATORS = True
